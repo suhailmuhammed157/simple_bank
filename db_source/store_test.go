f80f927be47ed10209b995d3f6fc37456b4bc996
Suhail MP <suhailmuhammed157@gmail.com>
2025-02-17 22:45:23 +0400

    add more test cases

--- db_source/store_test.go
+++ db_source/store_test.go
@@ -12,10 +12,74 @@ func TestTransferTx(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 
-	_, err := store.TransferTx(context.Background(), TransferTxParams{
-		FromAccountID: account1.ID,
-		ToAccountID:   account2.ID,
-		Amount:        10,
-	})
-	require.NoError(t, err)
+	n := 5
+
+	// var wg sync.WaitGroup
+
+	// wg.Add(n)
+
+	results := make(chan TransferTxResult, n)
+	errors := make(chan error, n)
+
+	amount := int64(10)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			// defer wg.Done()
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+			results <- result
+			errors <- err
+
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errors
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		//check transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.NotZero(t, transfer.ID)
+		require.Equal(t, transfer.FromAccountID, account1.ID)
+		require.Equal(t, transfer.ToAccountID, account2.ID)
+		require.Equal(t, transfer.Amount, amount)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		//check entries
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.NotZero(t, fromEntry.ID)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.NotZero(t, toEntry.ID)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+	}
+
+	// wg.Wait()
+	close(results)
+	close(errors)
+
 }
