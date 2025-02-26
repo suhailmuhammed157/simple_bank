608913a436ce9da9fea86a63299011621a5f16b0
Suhail MP <suhailmuhammed157@gmail.com>
2025-02-26 21:39:54 +0400

    integrate env file and load data

--- db_source/main_test.go
+++ db_source/main_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	driverName = "postgres"
-	dataSource = "postgres://root:password@localhost:5433/simple_bank?sslmode=disable"
+	"github.com/suhailmuhammed157/simple_bank/utils"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, configErr := utils.LoadConfig("../.")
+	if configErr != nil {
+		log.Fatal("Cannot load env")
+	}
 	var err error
-	testDB, err = sql.Open(driverName, dataSource)
+	testDB, err = sql.Open(config.DBDriver, config.DataSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database")
 	}
--- main.go
+++ main.go
@@ -7,23 +7,23 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/suhailmuhammed157/simple_bank/api"
 	"github.com/suhailmuhammed157/simple_bank/db_source"
-)
-
-const (
-	driverName    = "postgres"
-	dataSource    = "postgres://root:password@localhost:5433/simple_bank?sslmode=disable"
-	serverAddress = "127.0.0.1:8080"
+	"github.com/suhailmuhammed157/simple_bank/utils"
 )
 
 func main() {
-	conn, err := sql.Open(driverName, dataSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Config loading error")
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DataSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database")
 	}
 
 	store := db_source.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server")
 	}
