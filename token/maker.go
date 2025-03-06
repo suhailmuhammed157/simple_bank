04f482bb6c6f336fc3da653b35a46c0258a7f9f7
Suhail MP <suhailmuhammed157@gmail.com>
2025-03-06 23:19:55 +0400

    add jwt package and create interface for generating jwt token

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// need to implement all the methods in this interface to implement
+type Maker interface {
+	// creates new token
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	//verify the token
+	VerifyToken(token string) (*Payload, error)
+}
