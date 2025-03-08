789aa37a7515b6d3b269ccd6530c60bcacf66841
Suhail MP <suhailmuhammed157@gmail.com>
2025-03-09 00:04:59 +0400

    implement paseto

---
+++ token/paseto_maker.go
@@ -0,0 +1,49 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPasetoMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: size must be exactly %d length", chacha20poly1305.KeySize)
+	}
+	maker := &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+	return maker, nil
+}
+
+func (pasetoMaker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return pasetoMaker.paseto.Encrypt(pasetoMaker.symmetricKey, payload, nil)
+}
+
+// verify the token
+func (pasetoMaker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := pasetoMaker.paseto.Decrypt(token, pasetoMaker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
